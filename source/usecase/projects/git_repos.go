9294457cf80a603e24d9860b7e600076ba6d9314
vkyl <viktor.kyliukh@gmail.com>
2026-07-01 20:57:50 +0200

    filter existing

--- source/usecase/projects/git_repos.go
+++ source/usecase/projects/git_repos.go
@@ -9,10 +9,20 @@ import (
 
 const hostRoot = "/host"
 
-func (uc *UseCase) ListGitRepoPaths(_ context.Context) ([]string, error) {
+func (uc *UseCase) ListGitRepoPaths(ctx context.Context) ([]string, error) {
+	projects, err := uc.projects.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	registered := make(map[string]struct{}, len(projects))
+	for _, p := range projects {
+		registered[p.Dir] = struct{}{}
+	}
+
 	var paths []string
 
-	err := filepath.WalkDir(hostRoot, func(path string, d fs.DirEntry, err error) error {
+	err = filepath.WalkDir(hostRoot, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return filepath.SkipDir
 		}
@@ -22,7 +32,9 @@ func (uc *UseCase) ListGitRepoPaths(_ context.Context) ([]string, error) {
 		if d.Name() == ".git" {
 			repoDir := filepath.Dir(path)
 			homePath := strings.Replace(repoDir, hostRoot, "~", 1)
-			paths = append(paths, homePath)
+			if _, ok := registered[homePath]; ok {
+				paths = append(paths, homePath)
+			}
 			return filepath.SkipDir
 		}
 		return nil
